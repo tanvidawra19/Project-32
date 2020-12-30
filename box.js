99caa01b6ad5da5a7ec3fd1f423f90e15ad22839
tanvidawra19 <74242965+tanvidawra19@users.noreply.github.com>
2020-12-30 21:05:35 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,40 @@
+class Block{
+
+    constructor(x,y,width,height){
+        var options={
+            'restitution':0.4,
+            'friction':1.0,
+        }
+        this.Visiblity = 225;
+
+       this.body=Bodies.rectangle(x,y,width,height,options);
+       this.width = width;
+       this.height = height;
+       World.add(world,this.body);
+    }
+
+    score(){
+        if(this.Visiblity<0 && this.Visiblity> -105){
+          score++
+        }
+    }
+
+    display(){
+        if(this.body.speed<3){
+        var pos=this.body.position;
+        var angle =this.body.angle;
+        push();
+        translate(pos.x , pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        rect(0,0,this.width,this.height);
+        pop();
+        }
+        else{
+            World.remove(world,this.body);
+            push();
+            this.Visiblity = this.Visiblity-5;
+            pop();
+        }
+    }
+}
